0c13f3831d95d998f333d5c6e0746aeca542bd52
kpodrezova <knovik9999@gmail.com>
2024-10-05 16:11:57 +0300

    Add files via upload

---
+++ 1main.py
@@ -0,0 +1,15 @@
+# 1st programm
+print(9 ** 0.5 * 5)
+
+# 2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd programm
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th programm
+print((float('123.456')))
+print((float('123.456')) * 10)
+print((int(1234.56)) % 10 )
